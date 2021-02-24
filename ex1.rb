a56d5a73160c45ed9accbf4517990506cb55c9f3
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-02-24 15:34:21 -0300

    Primeiro commit com exercícios 1 à 11.

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+#puts "Hello Again!"
+#puts "I like typing this."
+#puts "Yay! Printing."
+#puts "This is fun."
+#puts "I'd muche rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "Olá, eu sou o Leonardo!"
---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# Este é um exemplo de comentário
+# Outra linha de comentário
+
+puts "I could have code like this." #Outro comentário
+
+puts "This won't run."
+
+puts "This will run."
