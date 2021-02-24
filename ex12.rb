bac64f5d040792433fa485dced8e86403079c947
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-02-24 15:56:10 -0300

    Incluídos exercícios 12 e 13.

--- ex12.rb
+++ ex12.rb
@@ -6,7 +6,12 @@ puts "A bigger number is #{bigger}."
 
 print "Give me a another number: "
 another = gets.chomp
-number = another.to_i
+number = another.to_f
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+print "Give some dollar: "
+dollar = gets.chomp.to_f
+
+puts "Ok, i refund you with #{dollar * 0.1}"
---
+++ ex13.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# to run, type, for exemple: "ruby ex13.rb first 2nd 3rd"
