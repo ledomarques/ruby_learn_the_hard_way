6ae8c87846693ec4d550621eb13fbe2121e68bba
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-03-09 09:17:19 -0300

    algumas brincadeiras a mais

--- ex1.rb
+++ ex1.rb
@@ -5,4 +5,13 @@ puts "Hello World!"
 #puts "This is fun."
 #puts "I'd muche rather you 'not'."
 #puts 'I "said" do not touch this.'
-puts "Olá, eu sou o Leonardo!"
+nome = "Leonardo"
+puts "Olá, eu sou o #{ nome }!"
+
+mensagem = <<~HDOC
+    Olá #{ nome },
+
+    bem vindo!!!
+HDOC
+puts mensagem
+
